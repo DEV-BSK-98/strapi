08189cf38aac2cea6cf09d1e684e4854d962fb0e
BSK-98 <nathaniellee20graham@gmail.com>
2025-05-05 11:20:46 +0200

    uodae

--- config/server.ts
+++ config/server.ts
@@ -1,7 +1,7 @@
 export default ({ env }) => ({
   host: '0.0.0.0',
   port: 1337,
-  // url: 'https://creditx.corestackpro.com',
+  url: 'https://creditx.corestackpro.com',
   app: {
     keys: env.array('APP_KEYS'),
   },
